aafc4ca558bb5bac6283bd97a38846c2eaad8575
edumangabeira <edu.mangaba@gmail.com>
2020-02-11 21:00:35 -0300

    removed test scripts

--- uTest/uTest_umtweetPorArquivo.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-import Preprocessor
-import umtweetPorArquivo
-
-folder = '/home/edu/portugueseTextPreprocessor/TweetPFile'
-arquivo = 'testing.txt'
-oneFilePerMessage(PreProcessor,folder,arquivo)
